a63df284ffacf940757775502b70b257e24b0562
FabioMassaretto <FabioMassaretto>
2016-09-29 01:10:57 -0300

    Criado página que contém menu da paciente

---
+++ menuAsidePaciente.php
@@ -0,0 +1,13 @@
+<h2>Heading</h2>
+<ul class="list-group">
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Informações Socioeconomicas da Paciente</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Detalhes da Gravidez</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Recém-Nascido</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Acompanhamentos da Gravidez</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Exames Físico Geral</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Exames Físico Obstétrico</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Exames Laboratoriais</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Resultado Paciente</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Amostras Paciente</a></li>
+  <li class="list-group-item"><span class="badge">0</span><a href="">Criar/Editar Resultado - Exames Laboratoriais</a></li>
+</ul>
