93d969a03ca8c9e15dc5d3b42554ac4d4d47d511
FabioMassaretto <FabioMassaretto>
2016-10-01 21:21:16 -0300

    Adicionado novos seletores

--- app/js/selectValues/selectPreValues.js
+++ app/js/selectValues/selectPreValues.js
@@ -4,12 +4,29 @@ angular
     $scope.simnao = [
       {"nome": " ", "value": " "},
       {"nome": "Sim", "value": "sim"},
-      {"nome": "Não", "value": "não"}
+      {"nome": "Não", "value": "nao"}
     ];
 
     $scope.melhoresHorarios = [
       {"nome": "Manhã", "value": "manha"},
       {"nome": "Tarde", "value": "tarde"},
       {"nome": "Noite", "value": "noite"}
+    ];
+
+    $scope.prateleiras = [
+      {"nome": " ", "value": " "},
+      {"nome": "Prateleira 1", "value": "Prateleira 1"},
+      {"nome": "Prateleira 2", "value": "Prateleira 2"},
+      {"nome": "Prateleira 3", "value": "Prateleira 3"},
+      {"nome": "Prateleira 4", "value": "Prateleira 4"},
+      {"nome": "Prateleira 5", "value": "Prateleira 5"},
+      {"nome": "Prateleira 6", "value": "Prateleira 6"}
+    ];
+
+    $scope.freezers = [
+      {"nome": " ", "value": " "},
+      {"nome": "Freezer 1", "value": "Freezer 1"},
+      {"nome": "Freezer 2", "value": "Freezer 2"},
+      {"nome": "Freezer 3", "value": "Freezer 3"}
     ];
   });
